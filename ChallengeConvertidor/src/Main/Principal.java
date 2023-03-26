fe5a97059601d034170f34d397474cfd50385fb8
Clever <mhugoclever@gmail.com>
2023-03-26 11:32:08 -0500

    Funciones de Moneda

--- ChallengeConvertidor/src/Main/Principal.java
+++ ChallengeConvertidor/src/Main/Principal.java
@@ -1,8 +1,35 @@
 package Main;
 
 import javax.swing.JOptionPane;
+
+import MonedasConvertir.Funcion;
 public class Principal {
 	public static void main(String[] args) {
-		
+		Funcion monedas = new Funcion();
+		String input = JOptionPane.showInputDialog("Ingresa la cantidad de dinero que deseas convertir: ");
+        if(ValidarNumero(input) == true) {
+            double Minput = Double.parseDouble(input);
+            monedas.ConvertirMonedas(Minput);
+
+            int respuesta=JOptionPane.showConfirmDialog(null,"¿Deseas realizar otra conversión?");
+            if (JOptionPane.OK_OPTION == respuesta){
+            	System.out.println("Selecciona opción Afirmativa");
+            }else{
+               	JOptionPane.showMessageDialog(null, "Programa terminado");                         
+            }
+
+           } else {
+               JOptionPane.showMessageDialog(null, "Valor inválido");                
+           }
+	
 	}
+	public static boolean ValidarNumero(String input) {
+        try {
+            double x = Double.parseDouble(input);
+            if(x >= 0 || x < 0);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+    }
 }
--- ChallengeConvertidor/src/MonedasConvertir/Funcion.java
+++ ChallengeConvertidor/src/MonedasConvertir/Funcion.java
@@ -13,6 +13,38 @@ public class Funcion {
     			JOptionPane.PLAIN_MESSAGE, null, new Object[] 
     			{"De Pesos a Dólar", "De Pesos a Euro", "De Pesos a Libras","De Pesos a Yen","De Pesos a Won Coreano","De Dólar a Pesos", "De Euro a Pesos", "De Libras a Pesos","De Yen a Pesos","De Won Coreano a Pesos"}, 
     			"Seleccion")).toString();
+		switch(opcion) {
+        case "De Pesos a Dólar":
+        	monedas.PesosDolares(Minput);
+        	break;
+        case "De Pesos a Euro":
+        	monedas.PesosEuros(Minput);
+        	break;
+        case "De Pesos a Libras":
+        	monedas.PesosLibras(Minput);
+        	break;
+        case "De Pesos a Yen":
+        	monedas.PesosYen(Minput);
+        	break;
+        case "De Pesos a Won Coreano":
+        	monedas.PesosWon(Minput);
+        	break;    	    	                          
+        case "De Dólar a Pesos":
+        	pesos.DolaresPesos(Minput);
+        	break;
+        case "De Euro a Pesos":
+        	pesos.EurosPesos(Minput);
+        	break;
+        case "De Libras a Pesos":
+        	pesos.LibroPesos(Minput);
+        	break;
+        case "De Yen a Pesos":
+        	pesos.YenPesos(Minput);
+        	break;
+        case "De Won Coreano a Pesos":
+            pesos.WonPesos(Minput);
+            break;
+        }
 		
 	}
 }
